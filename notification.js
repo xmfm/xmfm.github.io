a9f9fae828394b38299d59be343ec76e7ea5c675
小猫非猫 <30648725+xmfm@users.noreply.github.com>
2024-08-25 11:52:14 +0800

    Update notification.js

--- notification.js
+++ notification.js
@@ -24,7 +24,7 @@ window.addEventListener("load", () => {
       // 使用时间间隔以避免某些浏览器（包括 Firefox）在特定时间内出现过多通知时会阻止通知
       interval = setInterval(() => {
         // 由于 tag 参数，我们应该只能看到“Hi！9”通知
-        const n = new Notification(`Hi! ${i}`, { tag: "soManyNotification" });
+        const n = new Notification("通知", { body:"这是第${i}个", icon:"/favicon.ico", tag:"1", renotify:true});
         i++;
       }, 2000);
     } else {
