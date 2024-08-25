dc03389ffc93515ff3173181846478a51bced9f8
小猫非猫 <30648725+xmfm@users.noreply.github.com>
2024-08-25 13:17:40 +0800

    Update notification.js

--- notification.js
+++ notification.js
@@ -7,8 +7,6 @@ window.addEventListener("load", () => {
   buttonOff.style.height = "100px";
 
   var iterval = null;
-
-  Notification.requestPermission().then((status) => {});
   
   buttonOn.addEventListener("click", () => {
     if (Notification?.permission === "granted") {
@@ -22,9 +20,22 @@ window.addEventListener("load", () => {
         Notification.requestPermission(function(status) {var n=new Notification('123',{body:body});});
         i++;
       }, 2000);
+    } else if (Notification?.permission !== "denied"){
+        // 如果用户没有告诉他们是否想要收到通知（注意：由于 Chrome，我们不确定是否设置了权限属性），因此检查“默认”值是不安全的。
+        Notification.requestPermission().then((status) => {
+          // 如果用户同意
+          if (status === "granted") {
+            const n = new Notification(`授权成功`, {
+              tag: "重新点击开始键开始定期推送",
+            });
+          } else {
+            // 否则，我们可以回退到常规模式的提醒
+            alert("授权被拒绝，想继续使用请刷新重试");
+          }
+        });
     } else {
       // 否则，我们可以回退到常规模式的提醒
-      alert("授权被拒绝");
+      alert("授权被拒绝，想继续使用请刷新重试");
     }
   });
 
