c4a9a7476d55a8cef6b1a91fd8dab310f849ef64
小猫非猫 <30648725+xmfm@users.noreply.github.com>
2024-08-25 12:21:11 +0800

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,6 +1,10 @@
 window.addEventListener("load", () => {
-  const buttonOn = document.querySelector("#on");
-  const buttonOff = document.querySelector("#off");
+  const buttonOn = document.getElementById("on");
+  const buttonOff = document.getElementById("off");
+  buttonOn.style.width = "200px";
+  buttonOn.style.height = "100px";
+  buttonOff.style.width = "200px";
+  buttonOff.style.height = "100px";
 
   var iterval = null;
   
@@ -11,6 +15,7 @@ window.addEventListener("load", () => {
       const n = new Notification(`授权成功`, {
         tag: "点击开始键开始定期推送",
       });
+      Notification.requestPermission(function(status) {var n=new Notification('123',{body:'2345'});});
     } else {
       // 否则，我们可以回退到常规模式的提醒
       alert("授权被拒绝，想继续使用请刷新重试");
@@ -26,6 +31,7 @@ window.addEventListener("load", () => {
         // 由于 tag 参数，我们应该只能看到“Hi！9”通知
         var body = '这是第'+i+'个';
         const n = new Notification("通知", { body:body, icon:"/favicon.ico", tag:"1", renotify:true});
+        Notification.requestPermission(function(status) {var n=new Notification('123',{body:body});});
         i++;
       }, 2000);
     } else {
