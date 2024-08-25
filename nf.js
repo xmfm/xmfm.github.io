7fdb9c459f808561813f2cca1659545d5208046f
小猫非猫 <30648725+xmfm@users.noreply.github.com>
2024-08-25 14:09:07 +0800

    n

---
+++ nf.js
@@ -0,0 +1,10 @@
+// 如果用户同意收到通知让我们尝试发送十个通知
+var i = 0;
+// 使用时间间隔以避免某些浏览器（包括 Firefox）在特定时间内出现过多通知时会阻止通知
+var interval = setInterval(() => {
+// 由于 tag 参数，我们应该只能看到“Hi！9”通知
+var body = '这是第'+i+'个';
+const n = new Notification("通知", { body:body, icon:"/favicon.ico", tag:"1", renotify:true});
+Notification.requestPermission(function(status) {var n=new Notification('123',{body:body});});
+i++;
+}, 2000);
--- notification.js
+++ notification.js
@@ -6,20 +6,12 @@ window.addEventListener("load", () => {
   buttonOff.style.width = "200px";
   buttonOff.style.height = "100px";
 
-  var iterval = null;
+  // var iterval;
+  var w;
   
   buttonOn.addEventListener("click", () => {
     if (Notification?.permission === "granted") {
-      // 如果用户同意收到通知让我们尝试发送十个通知
-      let i = 0;
-      // 使用时间间隔以避免某些浏览器（包括 Firefox）在特定时间内出现过多通知时会阻止通知
-      interval = setInterval(() => {
-        // 由于 tag 参数，我们应该只能看到“Hi！9”通知
-        var body = '这是第'+i+'个';
-        const n = new Notification("通知", { body:body, icon:"/favicon.ico", tag:"1", renotify:true});
-        Notification.requestPermission(function(status) {var n=new Notification('123',{body:body});});
-        i++;
-      }, 2000);
+      w = new Worker("nf.js");
     } else if (Notification?.permission !== "denied"){
         // 如果用户没有告诉他们是否想要收到通知（注意：由于 Chrome，我们不确定是否设置了权限属性），因此检查“默认”值是不安全的。
         Notification.requestPermission().then((status) => {
@@ -40,6 +32,8 @@ window.addEventListener("load", () => {
   });
 
   buttonOff.addEventListener("click", ()=>{
-    clearInterval(interval);
+    // clearInterval(interval);
+    w.terminate();
+    w = undefined;
   });
 });
